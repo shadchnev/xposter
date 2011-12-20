a9ef55821b0a757b47f8c90459283ef4ecaee065
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2011-12-20 00:22:30 +0000

    added a success message and slightly modified poster injection

--- xposter.js
+++ xposter.js
@@ -3,21 +3,34 @@ $(document).ready(function() {
     twitter: {enabled: true, insertButton: updateTweetButton, click: function(){}},
     facebook: {enabled: false, insertButton: insertFacebookButton, click: clickFacebookTab}
   }
-  injectXPoster();
-  $('textarea.twitter-anywhere-tweet-box-editor').on("keyup", textareaChange);
+  injectXPoster();  
 })
 
 chrome.extension.onRequest.addListener( // fb posting is async, so we need this to learn that it was successful
   function(request, sender, sendResponse) {
     if (request.action == "fb_post_successful") {
+      // since we live in an isolated world and can't access the page's JS directly, we have to inject the code to execute it
+      var script = document.createElement('script');
+      script.setAttribute("type", "application/javascript");
+      script.textContent = "twttr.showMessage('Your Facebook update has been sent')";
+      document.body.appendChild(script); // run the script
+      document.body.removeChild(script); // clean up
       $(".twitter-anywhere-tweet-box-editor").val("");    
     }
   }
 );
 
-function injectXPoster() {
-  if (twitterBoxPresent()) 
-    injectTwitterInterface();
+function injectXPoster() {  
+  setInterval(function(){ // because Twitter redraws the interface after page load and sometimes after page load, so I never disable this timer    
+    if ((twitterBoxPresent()) && ($('.xposter').length == 0)) {      
+      console.log('injected');
+      $("<div />").addClass('xposter').insertBefore('.text-area');
+      insert('twitter');
+      insert('facebook');
+      $('textarea.twitter-anywhere-tweet-box-editor').on("keyup", textareaChange);
+      matchUI2State();
+  	}
+  }, 500); 
 }
 
 function textareaChange(event) { 
@@ -45,26 +58,15 @@ function clickFacebookTab(currentState) {
 }
 
 function insertFacebookButton() {
-  $('<a />').attr('href', '#').addClass('xfacebook-button btn primary-btn').text('Share on Facebook').appendTo('.tweet-button-sub-container').click(postToFacebook);
+  $('<a />').attr('href', '#').addClass('xfacebook-button btn primary-btn disabled').text('Share on Facebook').appendTo('.tweet-button-sub-container').click(postToFacebook);
 }
 
 function updateTweetButton() {
   $('.tweet-button').addClass('xtwitter-button'); // to have a universal naming scheme for all buttons
 }
 
-function injectTwitterInterface() {
-  var interval = setInterval(function(){ // because Twitter redraws the interface after page load
-    if ($('.xposter').length == 0) {      
-      console.log('injected');
-      // clearInterval(interval);
-      $("<div />").addClass('xposter').insertBefore('.text-area');
-      insert('twitter');
-      insert('facebook');
-      matchUI2State();
-  	}
-  }, 100); 
-}
 
+// this function relies on the states we defined at the beginning of the file
 function matchUI2State() {
   $('.tweet-button-sub-container .btn').hide();
   for (e in state) {
@@ -77,7 +79,7 @@ function matchUI2State() {
 }
 
 function selectTab(element) {
-	state[element].click(state[element].enabled);
+	state[element].click(state[element].enabled); // this calls the handler defined on the state in $(document).ready()
   if (state[element].enabled) return;
   for (e in state) state[e].enabled = false;
   state[element].enabled = true;
